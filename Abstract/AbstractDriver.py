b35535b1f5e60e70188e4d9cb851ef07a5a50516
d1rum <durmusakman98@gmail.com>
2023-10-27 15:07:49 +0300

    first commit

--- Abstract/AbstractDriver.py
+++ Abstract/AbstractDriver.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 class AbstractDriver(ABC):
 
     def __init__(self):
+
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
     def get_driver(self):
